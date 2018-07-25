82fa8ec6ac5d7e2ec212eaa85dfa434bf20114a1
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-07-25 14:37:25 -0700

    Add routes for the homepage and ride share pricing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+import { RideShareComparisonComponent } from './ride-share-comparison/ride-share-comparison.component';
+import { HomepageComponent } from './homepage/homepage.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent },
+  {path: 'ride_share', component: RideShareComparisonComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RideShareComparisonComponent } from './ride-share-comparison/ride-share-comparison.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    HomepageComponent,
+    RideShareComparisonComponent
   ],
   imports: [
     BrowserModule,
