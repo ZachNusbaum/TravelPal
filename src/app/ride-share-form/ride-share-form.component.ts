725aa514e017b6cdb4c4ba2e3c7ad06c3e5401f9
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-07-25 14:42:18 -0700

    Create component for the form

---
+++ src/app/ride-share-form/ride-share-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ride-share-form',
+  templateUrl: './ride-share-form.component.html',
+  styleUrls: ['./ride-share-form.component.css']
+})
+export class RideShareFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
