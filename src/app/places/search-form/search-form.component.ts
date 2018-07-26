5cf88d4f9a2bfe5203b2ebcc4c0a76980c92657a
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-07-25 22:00:10 -0700

    Add onSubmit event to places form

--- src/app/places/search-form/search-form.component.ts
+++ src/app/places/search-form/search-form.component.ts
@@ -1,3 +1,5 @@
+import { GooglePlacesService } from './../../google-places.service';
+import { GeocodingService } from './../../geocoding.service';
 import { PlacesQuery } from './places-query';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchFormComponent implements OnInit {
   model = new PlacesQuery('');
+  results = null;
 
-  constructor() { }
+  constructor(private geocoder: GeocodingService, private places: GooglePlacesService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    let coords;
+    this.geocoder.geocodeAddress(this.model.address).subscribe((response: any) => {
+      coords = response.results[0].geometry.location;
+      console.log(coords);
+      this.places.placesNear(coords).subscribe((placesResponse: any) => {
+        console.log(placesResponse);
+        this.results = placesResponse.results;
+      });
+    });
   }
 
 }
