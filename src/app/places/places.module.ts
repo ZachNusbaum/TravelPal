4d1f61ed195b534f294d2661a553217bf8e18a2b
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-07-25 22:00:44 -0700

    Add component for places list

--- src/app/places/places.module.ts
+++ src/app/places/places.module.ts
@@ -4,13 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { PlacesPageComponent } from './places-page/places-page.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
-  declarations: [SearchFormComponent, PlacesPageComponent],
+  declarations: [SearchFormComponent, PlacesPageComponent, ListComponent],
   providers: [GooglePlacesService]
 })
 export class PlacesModule { }
