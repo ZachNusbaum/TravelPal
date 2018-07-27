7fb29146846fd98ab0d98d6dc7daa200acd0e2e8
Zach Nusbaum <zach@zdnmail.com>
2018-07-27 14:55:03 -0700

    Price share tool now triggers an alert if the addresses are more than 100 miles apart.

--- src/app/price-lists/price-lists.component.ts
+++ src/app/price-lists/price-lists.component.ts
@@ -38,7 +38,8 @@ export class PriceListsComponent implements OnInit {
       // There are two responses, one for each request in the forkJoin.
       this.coords1 = responses[0].results[0].geometry.location;
       this.coords2 = responses[1].results[0].geometry.location;
-
+      let dist = this.distance(this.coords1.lat, this.coords1.lng, this.coords2.lat, this.coords2.lng, 'M');
+      if ( dist > 100 ) { alert('Error: Addresses are more than 100 miles apart.'); this.loading = false; return false; }
       this.loading = false; // At this point, the addresses are geocoded.
       this.geocoded = true;
       console.log('Geocoded: ', [this.coords1, this.coords2]);
@@ -70,6 +71,51 @@ export class PriceListsComponent implements OnInit {
       console.log(error);
     });
 
+  }
+
+    //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  //:::                                                                         :::
+  //:::  This routine calculates the distance between two points (given the     :::
+  //:::  latitude/longitude of those points). It is being used to calculate     :::
+  //:::  the distance between two locations using GeoDataSource (TM) prodducts  :::
+  //:::                                                                         :::
+  //:::  Definitions:                                                           :::
+  //:::    South latitudes are negative, east longitudes are positive           :::
+  //:::                                                                         :::
+  //:::  Passed to function:                                                    :::
+  //:::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :::
+  //:::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :::
+  //:::    unit = the unit you desire for results                               :::
+  //:::           where: 'M' is statute miles (default)                         :::
+  //:::                  'K' is kilometers                                      :::
+  //:::                  'N' is nautical miles                                  :::
+  //:::                                                                         :::
+  //:::  Worldwide cities and other features databases with latitude longitude  :::
+  //:::  are available at https://www.geodatasource.com                          :::
+  //:::                                                                         :::
+  //:::  For enquiries, please contact sales@geodatasource.com                  :::
+  //:::                                                                         :::
+  //:::  Official Web site: https://www.geodatasource.com                        :::
+  //:::                                                                         :::
+  //:::               GeoDataSource.com (C) All Rights Reserved 2017            :::
+  //:::                                                                         :::
+  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+  distance(lat1, lon1, lat2, lon2, unit) {
+    let radlat1 = Math.PI * lat1/180;
+    let radlat2 = Math.PI * lat2/180;
+    let theta = lon1-lon2;
+    let radtheta = Math.PI * theta/180;
+    let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+      dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = dist * 180 / Math.PI;
+    dist = dist * 60 * 1.1515;
+    if (unit === 'K') { dist = dist * 1.609344; }
+    if (unit === 'N') { dist = dist * 0.8684; }
+    return dist;
   }
 
 }
