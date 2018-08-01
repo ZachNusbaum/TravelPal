195f2e1b55d1f2b12655b934502fad14ae048bd9
Zach Nusbaum <zach@zdnmail.com>
2018-07-31 19:49:51 -0700

    Add reverse geocoding to the places page

--- src/app/places/places-page/places-page.component.ts
+++ src/app/places/places-page/places-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ReverseGeocodeService } from './../../reverse-geocode.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SearchFormComponent } from '../search-form/search-form.component';
 
 @Component({
   selector: 'app-places-page',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./places-page.component.css']
 })
 export class PlacesPageComponent implements OnInit {
+  @ViewChild(SearchFormComponent) searchForm: SearchFormComponent;
+  userAddress: string;
 
-  constructor() { }
+  constructor(private rg: ReverseGeocodeService) { }
 
   ngOnInit() {
+    this.getLocation();
+  }
+
+  getLocation() {
+    if (localStorage.getItem('start')) { return true; }
+    if ('geolocation' in navigator) {
+      console.log('Navigator!');
+      navigator.geolocation.getCurrentPosition((position: any) => {
+        this.rg.reverseGeocode({lat: position.coords.latitude, lng: position.coords.longitude}).subscribe((response: any) => {
+          console.log(response);
+          this.userAddress = response.results[0].formatted_address;
+          this.searchForm.model.address = this.userAddress;
+          console.log(this.userAddress);
+        });
+      });
+    } else {
+      console.log('Geolocation not available');
+    }
   }
 
 }
